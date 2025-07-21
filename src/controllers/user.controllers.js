109037d79dde3ca4bc9624b7ec08a93c4f623517
vikasya7 <vikasyaa19@gmail.com>
2025-07-21 13:53:24 +0530

    fix: correct import casing for ApiResponse in user.controllers.js

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,7 +1,7 @@
 import { User } from "../models/user.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import {asyncHandler} from "../utils/asyncHandler.js"
-import { ApiResponse } from "../utils/apiResponse.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 import { Report } from "../models/report.models.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
